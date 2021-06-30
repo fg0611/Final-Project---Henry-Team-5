35c6682c95236ae03d04136f0e090ad23263cd58
francisco garrido <fg0611@gmail.com>
2021-06-30 19:57:53 -0300

    Back End Boilerplate Update
    100% working

--- back/src/app.js
+++ back/src/app.js
@@ -8,7 +8,7 @@ require("./db.js");
 
 const server = express();
 
-server.name = "BACK";
+server.name = "API";
 
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
